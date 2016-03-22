8b660ec44254ba8ed445a391d3d9872380627b23
Eric Arlan Dockery <eric.dockery283@gmail.com>
2016-03-22 10:26:32 -0400

    Added ObjectiveC challenge

---
+++ CoreOSChallenge/ObjectiveCChallenge/PokeDex/PokemonStarter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PokemonStarter
+//
+//  Created by Eric Dockery on 2/15/16.
+//  Copyright © 2016 Eric Dockery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
